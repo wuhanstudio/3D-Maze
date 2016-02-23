7196f1f4d830f8f5fe8e4a53e16b49cef5eda2f2
gordon adams <gordonadamsnz@gmail.com>
2016-02-24 11:19:27 +1300

    Square map working and moving
    odd position error with XY <= 9,9

--- testing/minimap/index.js
+++ testing/minimap/index.js
@@ -1,18 +1,19 @@
 var squares = [];
 var svg;
-var cellSize = 20;
+var cellSize = 13;
 var xOffset = 5, yOffset = 5;
-var viewWindow = 10;
+var viewWindow = 17;
 
-var width = 10, height = 10;
+var width = 20, height = 20;
 var player = {x: width|1 , y: height|1};
 var board = makeMaze(width, height);//.reduce(function(prev, cur) {return prev.concat(cur);});
 
 function drawMap(){
-    svg = d3.selectAll("body")
+    svg = d3.selectAll(".miniMap")
             .selectAll("svg")
             .data([0]);
-    svgWidth = svgHeight = viewWindow * cellSize;
+    svgWidth = viewWindow * cellSize;
+    svgHeight = viewWindow * cellSize;
     svg.enter()
        .append("svg")
        .attr("width", svgWidth)
@@ -21,28 +22,32 @@ function drawMap(){
 
     var data = (function() {
         var firstCol = player.x-(viewWindow/2), lastCol = player.x+(viewWindow/2)+1;
+        firstCol = (firstCol >= 0 ? firstCol : 0);
+        lastCol = (lastCol >= board.length-1 ? lastCol : board.length-1);
         // lastCol-firstCol === viewWindow;
         // player.x === middleRow
         var firstRow = player.y-(viewWindow/2), lastRow = player.y+(viewWindow/2)+1;
-        console.clear();
-        console.log("Player:" + player.x + "|" + player.y, "firstCol: " + firstCol, "lastCol: " + lastCol, "firstRow: " + firstRow, "lastRow: " + lastRow);
+        firstRow = (firstRow >= 0 ? firstRow : 0);
+        lastRow = (lastRow >= board[0].length-1 ? lastRow : board[0].length-1);
+        //console.log("Player:" + player.x + "|" + player.y, "firstCol: " + firstCol, "lastCol: " + lastCol, "firstRow: " + firstRow, "lastRow: " + lastRow);
         // var last = player.x+3; **************************************************Not Used?
-        return board.slice(firstCol,lastCol).map(function(arr, c) {
+        slicedBoard = board.slice(firstCol,lastCol).map(function(arr, c) {
             var col = c;
             return arr.slice(firstRow,lastRow).map(function(val, row) {
                 return {value: val, position: [(firstCol+c),(firstRow+row)]}; //position == position on board
             });
-        }).reduce(function(prev, cur) {
-            return prev.concat(cur);
         });
+        return slicedBoard.reduce(function(prev, cur) {
+            return prev.concat(cur);
+        }, []);
     })();
 
-    var spaces = svg.selectAll("rect").data(data, function(d) {return d.position[0]*10+d.position[1];});
+    var spaces = svg.selectAll("rect").data(data, function(d) {return d.position[0]*100+d.position[1];});
 
     spaces.enter().append('rect');
     spaces.attr({
-            x: function(d) {return (d.position[0] - player.x - 1) * cellSize + svgWidth/2;},
-            y: function(d) {return (d.position[1] - player.y - 1) * cellSize + svgHeight/2;},
+            y: function(d) {return (d.position[0] - player.x - 1) * cellSize + svgWidth/2;},
+            x: function(d) {return (d.position[1] - player.y - 1) * cellSize + svgHeight/2;},
             width: cellSize,
             height: cellSize,
             class: function(d) {return (d.value=='_' ? "path" : "");},
@@ -74,16 +79,16 @@ window.onkeypress = function(e){
     var key = e.keyCode ? e.keyCode : e.which;
     switch (key) {
         case 119: //w
-            player.y--;
-            break;
-        case 97: //a
             player.x--;
             break;
+        case 97: //a
+            player.y--;
+            break;
         case 115: //s
-            player.y++;
+            player.x++;
             break;
         case 100: //d
-            player.x++;
+            player.y++;
             break;
         default:
         console.log(key + " pressed");
